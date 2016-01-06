f696714e1f9c7acda028f543d3451b1a384d0e8d
Sajjad Taheri <sajjadtaheri@dhcp-v243-115.pv.reshsg.uci.edu>
2016-01-05 18:29:03 -0800

    fixing things

--- test/test_features2d.js
+++ test/test_features2d.js
@@ -1,7 +1,7 @@
 /*/////////////////////////////////////////////////////////////////////////////
 AUTHOR: Sajjad Taheri sajjadt[at]uci[at]edu
 
-                           LICENSE AGREEMENT
+                             LICENSE AGREEMENT
 Copyright (c) 2015, University of california, Irvine
 
 Redistribution and use in source and binary forms, with or without
@@ -13,15 +13,15 @@ modification, are permitted provided that the following conditions are met:
    documentation and/or other materials provided with the distribution.
 3. All advertising materials mentioning features or use of this software
    must display the following acknowledgement:
-   This product includes software developed by the <organization>.
-4. Neither the name of the <organization> nor the
+   This product includes software developed by the UC Irvine.
+4. Neither the name of the UC Irvine nor the
    names of its contributors may be used to endorse or promote products
    derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY UC Irvine ''AS IS'' AND ANY
+THIS SOFTWARE IS PROVIDED BY UC IRVINE ''AS IS'' AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL UC Irvine or contributors BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL UC IRVINE OR CONTRIBUTORS BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
--- test/test_io.js
+++ test/test_io.js
@@ -13,15 +13,15 @@ modification, are permitted provided that the following conditions are met:
    documentation and/or other materials provided with the distribution.
 3. All advertising materials mentioning features or use of this software
    must display the following acknowledgement:
-   This product includes software developed by the <organization>.
-4. Neither the name of the <organization> nor the
+   This product includes software developed by the UC Irvine.
+4. Neither the name of the UC Irvine nor the
    names of its contributors may be used to endorse or promote products
    derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY UC Irvine ''AS IS'' AND ANY
+THIS SOFTWARE IS PROVIDED BY UC IRVINE ''AS IS'' AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL UC Irvine or contributors BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL UC IRVINE OR CONTRIBUTORS BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -30,63 +30,3 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /////////////////////////////////////////////////////////////////////////////*/
-
-module ("IO")
-QUnit.test("Test IO", function(assert) {
-	const CV_LOAD_IMAGE_COLOR = 1;
-	// Imwrite/Imread
-	{
-		  let mat = new cv.Mat(48, 64, cv.CV_8UC4);
-			Commons.createAlphaMat(mat);
-
-			compressionParams = new cv.IntVector();
-			compressionParams.push_back(16);
-			compressionParams.push_back(9);
-
-			cv.imwrite("alpha.png", mat, compressionParams);
-
-			let mat2 = cv.imread("alpha.png", 1); // RGB
-			assert.equal(mat.total(), mat2.total());
-			assert.equal(mat2.channels(), 3);
-
-			let mat3 = cv.imread("alpha.png", 0); //Grayscale
-			assert.equal(mat.total(), mat3.total());
-			assert.equal(mat3.channels(), 1);
-
-
-			mat.delete();
-			mat2.delete();
-			mat3.delete();
-			compressionParams.delete();
-	}
-	// Imencode/Imdecode
-	{
-			let mat = new cv.Mat(480, 640, cv.CV_8UC4),
-					buff = new cv.UCharVector(),
-					param = new cv.IntVector();
-
-			Commons.createAlphaMat(mat);
-			param.push_back(1); // CV_IMWRITE_JPEG_QUALITY
-			param.push_back(95);
-			cv.imencode(".png", mat, buff, param);
-
-			let mat2 = cv.imdecode(new cv.Mat(buff), CV_LOAD_IMAGE_COLOR);
-
-			assert.equal(mat.total(), mat2.total())
-
-			mat.delete();
-			buff.delete();
-			mat2.delete();
-			param.delete();
-	}
-	// Show image
-	{
-			let mat = new cv.Mat([50, 50], cv.CV_8UC4);
-
-			Commons.createAlphaMat(mat);
-			Commons.showImage(mat);
-
-			mat.delete();
-	}
-
-});
