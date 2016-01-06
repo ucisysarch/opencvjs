eb1890dc7bb34fe05e646fa063da50db23c3363e
Sajjad Taheri <sajjadtaheri@dhcp-v243-115.pv.reshsg.uci.edu>
2016-01-05 19:48:42 -0800

    tests changed

--- test/test_commons.js
+++ test/test_commons.js
@@ -32,22 +32,22 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /////////////////////////////////////////////////////////////////////////////*/
 
 var Commons = {
-  // Utility functions
+	// Utility functions
 	clamp: function(num, min, max) {
 		return num < min ? min : num > max ? max : num;
 	},
 
 	createAlphaMat: function(mat) {
-			let UCHAR_MAX =  255;
-			for (var i = 0; i < mat.rows; ++i) {
-					for (var j = 0; j < mat.cols; ++j) {
-							var rgba = mat.ptr(i, j);
-							rgba[0] = this.clamp((mat.rows - i)/(mat.rows) * UCHAR_MAX, 0, UCHAR_MAX); // Red
-							rgba[1] = this.clamp((mat.cols - j)/(mat.cols) * UCHAR_MAX, 0, UCHAR_MAX); // Green
-							rgba[2] = UCHAR_MAX; // Blue
-							rgba[3] = this.clamp(0.5 * (rgba[1] + rgba[2]), 0, UCHAR_MAX); // Alpha
-					}
+		let UCHAR_MAX =  255;
+		for (var i = 0; i < mat.rows; ++i) {
+			for (var j = 0; j < mat.cols; ++j) {
+				var rgba = mat.ptr(i, j);
+				rgba[0] = this.clamp((mat.rows - i)/(mat.rows) * UCHAR_MAX, 0, UCHAR_MAX); // Red
+				rgba[1] = this.clamp((mat.cols - j)/(mat.cols) * UCHAR_MAX, 0, UCHAR_MAX); // Green
+				rgba[2] = UCHAR_MAX; // Blue
+				rgba[3] = this.clamp(0.5 * (rgba[1] + rgba[2]), 0, UCHAR_MAX); // Alpha
 			}
+		}
 	},
 
 	showImage: function(mat) {
@@ -55,7 +55,6 @@ var Commons = {
 		canvas.style.zIndex   = 8;
 		canvas.style.border   = "1px solid";
 		document.body.appendChild(canvas);
-
 
 		ctx = canvas.getContext("2d");
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -65,8 +64,8 @@ var Commons = {
 
 		let imdata = ctx.createImageData(mat.cols, mat.rows);
 		let data = mat.data(),
-				channels = mat.channels(),
-				channelSize = mat.elemSize1();
+		channels = mat.channels(),
+		channelSize = mat.elemSize1();
 
 		for (var i = 0, j=0; i < data.length; i += channels, j+=4) {
 			imdata.data[j] = data[i];
